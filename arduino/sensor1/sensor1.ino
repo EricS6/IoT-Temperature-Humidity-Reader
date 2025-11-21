92aec064eae8f363ac7405191b0185a16568b813
EricS6 <estanski@icloud.com>
2025-11-21 15:40:21 -0500

    Add sensor reader

---
+++ arduino/sensor1/sensor1.ino
@@ -0,0 +1,27 @@
+#include <DHT.h>
+#define DHTPIN 2    // Sensor data pin
+#define DHTTYPE DHT11 // Sensor type
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  float temp = dht.readTemperature(true);
+  float hum = dht.readHumidity();
+
+  if(isnan(temp) || isnan(hum)) {
+    Serial.println("Sensor error");
+    delay(2000);
+    return;
+  }
+
+  Serial.print("{\"temperature\": ");
+  Serial.print(temp);
+  Serial.print(", \"humidity\": ");
+  Serial.print(hum);
+  Serial.println("}");
+  delay(2000); // Delay 2 seconds
+}
